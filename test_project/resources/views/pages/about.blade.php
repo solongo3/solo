d568734e884e8dfaf995273974f4bfc23eeee52c
solongo3 <115098880+solongo3@users.noreply.github.com>
2023-09-10 18:08:02 +0800

    first commit

---
+++ test_project/resources/views/pages/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'Solongojav')}}</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+       
+    </head>
+    <body class="antialiased">
+        <h1> About </h1>
+        <p> Hi there</p>
+    </body>
+</html>
